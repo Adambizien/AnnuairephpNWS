b8dc29d3fb560fbbe03a0baa57483ff8927432c2
Adambizien/blog-symfony <bizienadam@outlook.com>
2023-10-12 19:47:28 +0200

    update : recuperation des détails du contacte

--- templates/page/detail.page.php
+++ templates/page/detail.page.php
@@ -15,7 +15,7 @@
         <div class="row">
             <div class="col-xl-12">
                 <div class="card mb-4">
-                    <div class="card-header text-center">Détails</div>
+                    <div class="card-header text-center">Détails <a href="./?page=formmodifier&layout=html&id=<?= $res[0]["id"] ?>" ><i class="bi bi-pencil-square"></i></a></div>
                     <div class="card-body">
                         <fieldset disabled>
                             <div class="row gx-3 mb-3">
@@ -25,7 +25,7 @@
                                         <div class="input-group-prepend">
                                             <div class="input-group-text"><i class="bi bi-person"></i></div>
                                         </div>
-                                        <input class="form-control" id="surname" type="text"  value="">
+                                        <input class="form-control" id="surname" type="text"  value="<?php echo (isset($res[0]['surname']) ? $res[0]['surname']:'')?>">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
@@ -34,7 +34,7 @@
                                         <div class="input-group-prepend">
                                             <div class="input-group-text"><i class="bi bi-person"></i></div>
                                         </div>
-                                        <input class="form-control" id="name" type="text"  value="">
+                                        <input class="form-control" id="name" type="text"  value="<?php echo (isset($res[0]['name']) ? $res[0]['name']:'')?>">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
@@ -43,7 +43,7 @@
                                         <div class="input-group-prepend">
                                             <div class="input-group-text"><i class="bi bi-calendar4"></i></div>
                                         </div>
-                                        <input class="form-control" id="birthDay" type="text"  value="">
+                                        <input class="form-control" id="birthDay" type="date"  value="<?php echo (isset($res[0]['birthday']) ? $res[0]['birthday']:'')?>">
                                     </div>
                                 </div>
                             </div>
@@ -54,7 +54,7 @@
                                         <div class="input-group-prepend">
                                             <div class="input-group-text"><i class="bi bi-envelope"></i></div>
                                         </div>
-                                        <input class="form-control" id="email" type="text" value="">  
+                                        <input class="form-control" id="email" type="text" value="<?php echo (isset($res[0]['email']) ? $res[0]['email']:'')?>">  
                                     </div>
                                 </div>
                                 <div class="col-md-6">
@@ -63,7 +63,7 @@
                                         <div class="input-group-prepend">
                                             <div class="input-group-text"><i class="bi bi-telephone"></i></div>
                                         </div>
-                                        <input class="form-control" id="phone" type="text" value="">
+                                        <input class="form-control" id="phone" type="text" value="<?php echo (isset($res[0]['phone']) ? $res[0]['phone']:'')?>">
                                     </div>
                                 </div>
                             </div>
@@ -75,7 +75,7 @@
                                         <div class="input-group-prepend">
                                             <div class="input-group-text"><i class="bi bi-house"></i></div>
                                         </div>
-                                        <input class="form-control" id="address" type="text"  value="">
+                                        <input class="form-control" id="address" type="text"  value="<?php echo (isset($res[0]['address']) ? $res[0]['address']:'')?>">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
@@ -84,7 +84,7 @@
                                         <div class="input-group-prepend">
                                             <div class="input-group-text"><i class="bi bi-mailbox-flag"></i></div>
                                         </div>
-                                        <input class="form-control" id="postalcode" type="text"  value="">
+                                        <input class="form-control" id="postalcode" type="text"  value="<?php echo (isset($res[0]['postalcode']) ? $res[0]['postalcode']:'')?>">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
@@ -93,13 +93,13 @@
                                         <div class="input-group-prepend">
                                             <div class="input-group-text"><i class="bi bi-geo-alt"></i></div>
                                         </div>
-                                        <input class="form-control" id="city" type="text"  value="">
+                                        <input class="form-control" id="city" type="text"  value="<?php echo (isset($res[0]['city']) ? $res[0]['city']:'')?>">
                                     </div>
                                 </div>
                             </div>
                             <div class="mb-3">
                                 <label class="small mb-1" for="description">Description :</label>
-                                <textarea class="form-control" id="description" rows="3" ></textarea>
+                                <textarea class="form-control" id="description" rows="3" ><?php echo ( isset($res[0]['description']) ? $res[0]['description'] : '');?></textarea>
                             </div>
                         </fieldset>
                     </div>
